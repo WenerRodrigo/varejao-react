7cd59786cbcd4cbabcd9b0be3d0fa4731b84df40
Wener Rodrigo <wener19.wr@gmail.com>
2023-03-21 21:43:35 -0300

    criando o arquivo common

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -9,8 +9,11 @@ import {
   InputLabel,
   InputAdornment 
 } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+import { UsuarioContext } from 'common/context/Usuario';
 
-function Login() {
+function Login({ nome, setNome, saldo, setSaldo }) {
+  const history = useHistory();
   return (
     <Container>
       <Titulo>
@@ -21,7 +24,9 @@ function Login() {
           Nome
         </InputLabel>
         <Input
-          type="text"
+        value={nome}
+        onChange={(event) => setNome=(event.target.value)}
+        type="text"
         />
       </InputContainer>
       <InputContainer>
@@ -30,6 +35,8 @@ function Login() {
         </InputLabel>
         <Input
         type="number"
+        value={saldo}
+        onChange={(event) => setSaldo=(event.target.value)}
         startAdornment={
           <InputAdornment position="start">
             R$
@@ -40,6 +47,7 @@ function Login() {
       <Button
         variant="contained"
         color="primary"
+        onClick={() => history.push('/feira')}
       >
         Avançar
       </Button>
